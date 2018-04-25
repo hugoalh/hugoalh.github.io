3b4ec85597b564512469a8e53c0069fe551d351b
hugoalh <hugoalh@gmail.com>
2018-04-25 19:07:31 +0800

    Add files via upload

---
+++ assets/language/language.js
@@ -0,0 +1,20 @@
+var langs = ['en', 'zh-Hant', 'zh-TW'];
+var langCode = '';
+var langJS = null;
+
+var translate = function (jsdata)
+{
+	$("[tkey]").each (function (index)
+	{
+		var strTr = jsdata [$(this).attr ('tkey')];
+		$(this).html (strTr);
+	}
+	);
+}
+
+langCode = navigator.language.substr (0, 2);
+
+if (langCode in langs)
+	$.getJSON('assets/language/'+langCode+'.json', translate);
+else
+	$.getJSON('assets/language/en.json', translate);
