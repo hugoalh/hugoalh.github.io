2ddf15b96f0004caf0b92bb4ce8fbbec76fa4620
hugoalh <hugoalh@gmail.com>
2018-04-26 12:19:23 +0800

    Update language.js

--- assets/language/language.js
+++ assets/language/language.js
@@ -1,4 +1,4 @@
-var language_provide = ['en', 'ch', 'chs'];
+/* var language_provide = ['en', 'ch', 'chs'];
 var langJS = null;
 
 var translate = function (jsdata)
@@ -27,3 +27,24 @@ if (language_code in language_provide) {
 } else {
 	$.getJSON('assets/language/en.json', translate);
 }
+*/
+var langs = ['en', 'ch'];
+var langCode = '';
+var langJS = null;
+
+var translate = function (jsdata)
+{
+	$("[tkey]").each (function (index)
+	{
+		var strTr = jsdata [$(this).attr ('tkey')];
+		$(this).html (strTr);
+	}
+	);
+}
+
+langCode = navigator.language.substr (0, 2);
+
+if (langCode in langs)
+	$.getJSON('assets/language/'+langCode+'.json', translate);
+else
+	$.getJSON('assets/language/en.json', translate);
