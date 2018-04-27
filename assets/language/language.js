e7718ac885583f17af22b48153d6ab45c9749cde
hugoalh <hugoalh@gmail.com>
2018-04-27 15:38:23 +0800

    Update language.js

--- assets/language/language.js
+++ assets/language/language.js
@@ -10,8 +10,11 @@ var translate = function (jsdata) {
 var language_detect = navigator.language.toLowerCase() || navigator.userLanguage.toLowerCase() || navigator.browserLanguage.toLowerCase();
 
 /* Simplify/Redirect Language Code*/
-if (language_detect == "zh-tw" || language_detect == "zh-hk" || language_detect == "zh-hant" ) {
+if (language_detect == "zh-hk" || language_detect == "zh-mo" || language_detect == "zh-sg" || language_detect == "zh-tw" || language_detect == "zh-hant" ) {
 	var language_set = "zh";
+};
+if (language_detect == "zh-cn" || language_detect == "zh-hans" ) {
+	var language_set = "zhs";
 }
 
 /* Get Language String In JSON */
@@ -22,5 +25,5 @@ if (language_set == "zh" ) {
 		$.getJSON("assets/language/zhs.json", translate);
 	} else {
 		$.getJSON("assets/language/en.json", translate);
-	}
+	};
 }
