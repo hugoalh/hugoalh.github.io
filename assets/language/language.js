da4345deaf4eac81e0f743bbdad5352668bff528
hugoalh <hugoalh@gmail.com>
2018-04-27 15:30:58 +0800

    Update language.js

--- assets/language/language.js
+++ assets/language/language.js
@@ -1,24 +1,20 @@
-/* var langJS = null; */
-
-var translate = function (jsdata)
-{
-	$("[languagestring]").each (function (index)
-	{
+/* Replace Language String */
+var translate = function (jsdata) {
+	$("[languagestring]").each (function (index) {
 		var strTr = jsdata [$(this).attr ('languagestring')];
 		$(this).html (strTr);
 	});
 };
 
+/* Get Client Language Code */
 var language_detect = navigator.language.toLowerCase() || navigator.userLanguage.toLowerCase() || navigator.browserLanguage.toLowerCase();
 
-/* if (detect_language == "zh-tw" || detect_language == "zh-hk" || detect_language == "zh-hant" ) {
-	$.getJSON("assets/language/ch.json", translate);
-} else {
-	$.getJSON("assets/language/en.json", translate);
-} */
+/* Simplify/Redirect Language Code*/
 if (language_detect == "zh-tw" || language_detect == "zh-hk" || language_detect == "zh-hant" ) {
 	var language_set = "ch";
 }
+
+/* Get Language String In JSON */
 if (language_set == "ch" ) {
 	$.getJSON("assets/language/ch.json", translate);
 } else {
