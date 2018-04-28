7b632cef8018b288173cb65c9ebdb17f3ff73230
hugoalh <hugoalh@gmail.com>
2018-04-28 18:37:06 +0800

    Update language.js

--- assets/language/language.js
+++ assets/language/language.js
@@ -1,8 +1,8 @@
-/*
-@hugoalh
-*/
+// ==================================================
+// @hugoalh
+// ==================================================
 
-/* Function, Replace Language String */
+// Function, Replace Language String
 var translate = function (jsdata) {
 	$("[languagestring]").each (function (index) {
 		var strTr = jsdata [$(this).attr ('languagestring')];
@@ -10,7 +10,29 @@ var translate = function (jsdata) {
 	});
 };
 
-/* Get Client Language Code */
+// Function, Cookie, Set
+function cookie_set(cookie_section_name, cookie_section_value) {
+	var time_now = new Date();
+	var cookie_time_expire = time_now.setTime(time_now.getTime() + (365 * 24 * 60 * 60 * 1000));
+	document.cookie = cookie_section_name + "=" + cookie_section_value + ";expires=" + cookie_time_expire.toUTCString() + ";domain=hugoalh.github.com";
+};
+
+// Function, Cookie, Check
+/*
+function cookie_check() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+*/
+
+// Get Client Language Code
 var language_detect = navigator.language.toLowerCase();
 if (language_detect == "undefined") {
 	var language_detect = navigator.browserLanguage.toLowerCase();
@@ -22,7 +44,7 @@ if (language_detect == "undefined") {
 	}
 };
 
-/* Simplify/Redirect Language Code*/
+// Redirect Language Code
 if (language_detect == "zh-hk" || language_detect == "zh-mo" || language_detect == "zh-sg" || language_detect == "zh-tw" || language_detect == "zh-hant" ) {
 	var language_set = "zh";
 };
@@ -30,7 +52,7 @@ if (language_detect == "zh-cn" || language_detect == "zh-hans" ) {
 	var language_set = "zhs";
 };
 
-/* Get Language String In JSON */
+// Get Language String In JSON
 if (language_set == "zh" ) {
 	$.getJSON("assets/language/zh.json", translate);
 };
