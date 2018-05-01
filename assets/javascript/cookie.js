0de366d897a9372df46570ca77d3b108b52cd772
hugoalh <hugoalh@gmail.com>
2018-05-01 13:17:52 +0800

    Update cookie.js

--- assets/javascript/cookie.js
+++ assets/javascript/cookie.js
@@ -7,16 +7,16 @@ API:	JavaScript (ECMAScript 6/7), Cookie
 */
 
 /* Data, Set */
-function cookie_data_set(cookie_datasection_name, cookie_datasection_value) {
-	var cookie_time_expire = new Date();
-	cookie_time_expire.setTime(cookie_time_expire.getTime() + (365.25 * 24 * 60 * 60 * 1000));
-	var cookie_time_expire_utc = cookie_time_expire.toUTCString();
-	document.cookie = cookie_datasection_name + "=" + cookie_datasection_value + ";expires=" + cookie_time_expire_utc + ";domain=hugoalh.github.io";
+function cookie_set(cookie_data_name, cookie_data_value, cookie_data_expiretime) {
+	var cookie_data_expiretime_set = new Date();
+	cookie_data_expiretime_set.setTime(cookie_data_expiretime_set.getTime() + (cookie_data_expiretime * 24 * 60 * 60 * 1000));
+	var cookie_data_expiretime_utc = cookie_data_expiretime_set.toUTCString();
+	document.cookie = cookie_data_name + "=" + cookie_data_value + ";expires=" + cookie_data_expiretime_utc + ";domain=hugoalh.github.io;path=/";
 };
 
 /* Data, Get */
-function cookie_data_get(cookie_datasection_name) {
-	var cookie_datasection_name_search = cookie_datasection_name + "=";
+function cookie_get(cookie_data_name) {
+	var cookie_data_name_search = cookie_data_name + "=";
 	var cookie_decode = decodeURIComponent(document.cookie);
 	var cookie_array = cookie_decode.split(';');
 	for(var i = 0; i < cookie_array.length; i++) {
@@ -24,8 +24,8 @@ function cookie_data_get(cookie_datasection_name) {
 		while (c.charAt(0) == ' ') {
 			c = c.substring(1);
 		};
-		if (c.indexOf(cookie_datasection_name_search) == 0) {
-			return c.substring(cookie_datasection_name_search.length, c.length);
+		if (c.indexOf(cookie_data_name_search) == 0) {
+			return c.substring(cookie_data_name_search.length, c.length);
 		}
 	};
 	return "";
