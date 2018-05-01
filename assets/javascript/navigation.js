fb813e8727ed3fbd9ec1374bbe089e1259a26b96
hugoalh <hugoalh@gmail.com>
2018-05-01 18:52:04 +0800

    Create navigation.js

---
+++ assets/javascript/navigation.js
@@ -0,0 +1,22 @@
+/*
+==================================================
+hugoalh Website Navigation Toolkit
+Author:	hugoalh
+API:	JavaScript (ECMAScript 6/7), Cookie
+==================================================
+*/
+
+function navigation_open_standard() {
+	document.getElementById("navigation_left").style.width = "250px";
+	document.getElementById("body_content").style.marginLeft = "250px";
+};
+
+function navigation_open_mobile() {
+	document.getElementById("navigation_left").style.width = "100%";
+	document.getElementById("body_content").style.marginLeft = "100%";
+};
+
+function navigation_close() {
+	document.getElementById("navigation_left").style.width = "0px";
+	document.getElementById("body_content").style.marginLeft = "0px";
+}
