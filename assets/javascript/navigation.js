3d6eb497de668325feae03d827aee092fbff30c2
hugoalh <hugoalh@gmail.com>
2018-05-01 19:04:06 +0800

    Update navigation.js

--- assets/javascript/navigation.js
+++ assets/javascript/navigation.js
@@ -6,9 +6,11 @@ API:	JavaScript (ECMAScript 6/7), Cookie
 ==================================================
 */
 
+/* Navigation Control */
 function navigation_open_standard() {
-	document.getElementById("navigation_left").style.width = "250px";
-	document.getElementById("body_content").style.marginLeft = "250px";
+	document.getElementById("navigation_left").style.width = "384px";
+	document.getElementById("body_content").style.marginLeft = "384px";
+	cookie_set(setting_navigation, 1, 7);
 };
 
 function navigation_open_mobile() {
@@ -18,4 +20,11 @@ function navigation_open_mobile() {
 function navigation_close() {
 	document.getElementById("navigation_left").style.width = "0px";
 	document.getElementById("body_content").style.marginLeft = "0px";
-}
+	cookie_set(setting_navigation, 0, 7);
+};
+
+var setting_navigation = cookie_get("setting_navigation");
+if (setting_navigation == 0) {
+	navigation_close();
+} else {
+	
