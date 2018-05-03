b434d5897051219f37e730318d6322d4df77a554
hugoalh <hugoalh@gmail.com>
2018-05-03 19:14:07 +0800

    Create google_search.js

---
+++ assets/javascript/google_search.js
@@ -0,0 +1,9 @@
+(function() {
+	var cx = '008207840337707381853:janfdzcaxbq';
+	var gcse = document.createElement('script');
+	gcse.type = 'text/javascript';
+	gcse.async = true;
+	gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(gcse, s);
+})();
