35ccd0a829cef290791454cf2ccfc7cf3494ca9f
hugoalh <hugoalh@gmail.com>
2018-05-07 17:41:08 +0800

    Update navigation.js

--- assets/javascript/navigation.js
+++ assets/javascript/navigation.js
@@ -2,29 +2,17 @@
 ==================================================
 hugoalh Website Navigation Toolkit
 Author:	hugoalh
-API:	JavaScript (ECMAScript 6/7), Cookie
+API:	JavaScript (ECMAScript 6/7)
 ==================================================
 */
 
 /* Navigation Control */
-function navigation_open_standard() {
-	document.getElementById("navigation_left").style.width = "384px";
-	document.getElementById("body_content").style.marginLeft = "384px";
-	cookie_set(setting_navigation, 1, 7);
-};
-
-function navigation_open_mobile() {
+function navigation_left_open_mobile() {
 	document.getElementById("navigation_left").style.width = "100%";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 };
 
-function navigation_close() {
+function navigation_left_close_mobile() {
 	document.getElementById("navigation_left").style.width = "0px";
-	document.getElementById("body_content").style.marginLeft = "0px";
-	cookie_set(setting_navigation, 0, 7);
-};
-
-var setting_navigation = cookie_get("setting_navigation");
-if (setting_navigation == 0) {
-	navigation_close();
-} else {
-	
+	document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
