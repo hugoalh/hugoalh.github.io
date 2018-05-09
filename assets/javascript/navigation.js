0a9c6bced8203c7663e357057112457253e5a7f2
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-05-09 16:55:59 +0800

    UFGD

--- assets/javascript/navigation.js
+++ assets/javascript/navigation.js
@@ -7,12 +7,12 @@ API:		JavaScript (ECMAScript 6/7)
 */
 
 /* Navigation Control */
-function navigation_left_open_mobile() {
+function navigation_left_mobile_open() {
 	document.getElementById("navigation_left").style.width = "100%";
-	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
 };
 
-function navigation_left_close_mobile() {
+function navigation_left_mobile_close() {
 	document.getElementById("navigation_left").style.width = "0px";
 	document.body.style.backgroundColor = "rgba(0,0,0,0)";
 };
