35f6790d80789fa538ba9ebbeb40f2b640098dcd
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-05-10 11:03:50 +0800

    UFGD

--- assets/javascript/navigation.js
+++ assets/javascript/navigation.js
@@ -16,13 +16,3 @@ function navigation_left_mobile_close() {
 	document.getElementById("navigation_left").style.width = "0px";
 	document.body.style.backgroundColor = "rgba(0,0,0,0)";
 };
-
-document.getElementById("navigation_top").innerHTML = '
-	<a class="buttonmetroize" id="navigation_left_button" href="/"><i class="googlematerialicon googlematerialicon_24px googlematerialicon_light">menu</i></a>
-	<div id="navigation_websitelogo">
-		<img class="logo_website_navigation" src="assets/logo/hugoalh_tile.svg" />
-	</div>
-	<div id="navigaion_top_toolbox">
-		<a class="buttonmetroize"  href="/"><i class="googlematerialicon googlematerialicon_24px googlematerialicon_light">translate</i></a>
-	</div>
-';
