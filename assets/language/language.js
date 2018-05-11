40382ef1cbb8cc708c7f237f8af65323f83827fd
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-05-11 16:58:01 +0800

    UFGD

--- assets/language/language.js
+++ assets/language/language.js
@@ -1,70 +1,67 @@
-/*
-====================================================================================================
+/*==================================================================================================
 hugoalh's Website Language Extension
 Author:					hugoalh
 Programming Language:	JavaScript/ECMAScript 6/7, JSON, jQuery 1(.12.4)/2(.2.4)/3(.3.1), Cookie
-====================================================================================================
-*/
+==================================================================================================*/
+/*Replace Language String*/
+	var translate = function (jsdata) {
+		$("[languagestring]").each (function (index) {
+			var strTr = jsdata [$(this).attr ('languagestring')];
+			$(this).html (strTr);
+		});
+	};
 
-/* Replace String */
-var translate = function (jsdata) {
-	$("[languagestring]").each (function (index) {
-		var strTr = jsdata [$(this).attr ('languagestring')];
-		$(this).html (strTr);
-	});
-};
+/*Client Select Change Language To zh*/
+	function language_change_zh() {
+		cookie_set("setting_language", "zh", 365.25);
+		location.reload(true);
+	};
 
-/* Client Select Change To zh */
-function language_change_zh() {
-	cookie_set("setting_language", "zh", 365.25);
-	location.reload(true);
-};
+/*Client Select Change Language To zhs*/
+	function language_change_zhs() {
+		cookie_set("setting_language", "zhs", 365.25);
+		location.reload(true);
+	};
 
-/* Client Select Change To zhs */
-function language_change_zhs() {
-	cookie_set("setting_language", "zhs", 365.25);
-	location.reload(true);
-};
+/*Client Select Change Language To en*/
+	function language_change_en() {
+		cookie_set("setting_language", "en", 365.25);
+		location.reload(true);
+	};
 
-/* Client Select Change To en */
-function language_change_en() {
-	cookie_set("setting_language", "en", 365.25);
-	location.reload(true);
-};
-
-/* Check Cookie Setted Client Code */
+/*Check Cookie Setted Client Language Code*/
 var setting_language = cookie_get("setting_language");
 if (setting_language != "zh" && setting_language != "zhs" && setting_language != "en") {
-	/* Determine Client Code */
-	var language_detect = navigator.language.toLowerCase();
-	if (language_detect == "undefined") {
-		var language_detect = navigator.browserLanguage.toLowerCase();
+	/*Determine Client Language Code*/
+		var language_detect = navigator.language.toLowerCase();
 		if (language_detect == "undefined") {
-			var language_detect = navigator.userLanguage.toLowerCase();
+			var language_detect = navigator.browserLanguage.toLowerCase();
 			if (language_detect == "undefined") {
-				var language_detect = navigator.systemLanguage.toLowerCase();
+				var language_detect = navigator.userLanguage.toLowerCase();
+				if (language_detect == "undefined") {
+					var language_detect = navigator.systemLanguage.toLowerCase();
+				};
 			};
 		};
-	};
-	/* Redirect Code */
-	if (language_detect == "zh-hk" || language_detect == "zh-mo" || language_detect == "zh-sg" || language_detect == "zh-tw" || language_detect == "zh-hant" ) {
-		var setting_language = "zh";
-	};
-	if (language_detect == "zh-cn" || language_detect == "zh-hans" ) {
-		var setting_language = "zhs";
-	};
+	/*Redirect Language Code*/
+		if (language_detect == "zh-hk" || language_detect == "zh-mo" || language_detect == "zh-sg" || language_detect == "zh-tw" || language_detect == "zh-hant" ) {
+			var setting_language = "zh";
+		};
+		if (language_detect == "zh-cn" || language_detect == "zh-hans" ) {
+			var setting_language = "zhs";
+		};
 };
 
-/* Get String In JSON */
-if (setting_language == "zh" ) {
-	$.getJSON("/assets/language/zh.json", translate);
-	cookie_set("setting_language", "zh", 365.25);
-};
-if (setting_language == "zhs" ) {
-	$.getJSON("/assets/language/zhs.json", translate);
-	cookie_set("setting_language", "zhs", 365.25);
-};
-if (setting_language != "zh" && setting_language != "zhs") {
-	$.getJSON("/assets/language/en.json", translate);
-	cookie_set("setting_language", "en", 365.25);
-};
+/*Get Language String From JSON*/
+	if (setting_language == "zh" ) {
+		$.getJSON("/assets/language/zh.json", translate);
+		cookie_set("setting_language", "zh", 365.25);
+	};
+	if (setting_language == "zhs" ) {
+		$.getJSON("/assets/language/zhs.json", translate);
+		cookie_set("setting_language", "zhs", 365.25);
+	};
+	if (setting_language != "zh" && setting_language != "zhs") {
+		$.getJSON("/assets/language/en.json", translate);
+		cookie_set("setting_language", "en", 365.25);
+	};
--- assets/navigation/navigation.js
+++ assets/navigation/navigation.js
@@ -1,18 +1,14 @@
-/*
-====================================================================================================
+/*==================================================================================================
 hugoalh's Website Navigation Tool Set
 Author:					hugoalh
 Programming Language:	JavaScript/ECMAScript 6/7
-====================================================================================================
-*/
-
-/* Navigation Control */
-function navigation_left_mobile_open() {
-	document.getElementById("navigation_left").style.width = "100%";
-	document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
-};
-
-function navigation_left_mobile_close() {
-	document.getElementById("navigation_left").style.width = "0px";
-	document.body.style.backgroundColor = "rgba(0,0,0,0)";
-};
+==================================================================================================*/
+/*Navigation Control*/
+	function navigation_left_open() {
+		document.getElementById("navigation_left").style.width = "100%";
+		document.getElementById("page").style.backgroundColor = "rgba(0, 0, 0, 0.25)";
+	};
+	function navigation_left_close() {
+		document.getElementById("navigation_left").style.width = "0px";
+		document.getElementById("page").style.backgroundColor = "rgba(0, 0, 0, 0)";
+	};
