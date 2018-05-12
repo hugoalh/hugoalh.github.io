0e06340d1285504476c1f9a8e1d353ac3e0fa997
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-05-12 11:46:12 +0800

    Test

--- assets/navigation/navigation.js
+++ assets/navigation/navigation.js
@@ -1,7 +1,7 @@
 /*==================================================================================================
 hugoalh's Website Navigation Tool Set
 Author:					hugoalh
-Programming Language:	JavaScript/ECMAScript 6/7
+Programming Language:	JavaScript/ECMAScript 6/7, jQuery 1(.12.4)/2(.2.4)/3(.3.1)
 ==================================================================================================*/
 /*Navigation Control*/
 	function navigation_left_open() {
@@ -12,3 +12,6 @@ Programming Language:	JavaScript/ECMAScript 6/7
 		document.getElementById("navigation_left").style.width = "0px";
 		document.getElementById("page").style.backgroundColor = "rgba(0, 0, 0, 0)";
 	};
+
+/*Load Navigation*/
+	$("#navigation_top").load("/assets/navigation/data.html-external #navigation_top");
