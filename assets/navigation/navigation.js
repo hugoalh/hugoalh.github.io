ba29afe8a475380388bb0cf3c9c7f34f8214daf6
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-05-14 13:01:11 +0800

    Test

--- assets/navigation/navigation.js
+++ assets/navigation/navigation.js
@@ -6,7 +6,7 @@ Programming Language:	JavaScript/ECMAScript 6/7, jQuery 1(.12.4)/2(.2.4)/3(.3.1)
 /*Navigation Left Control*/
 	function navigation_left_open() {
 		document.getElementById("navigation_left").style.width = "100%";
-		document.getElementById("page").style.backgroundColor = "rgba(0, 0, 0, 0.25)";
+		document.getElementById("page").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
 		document.getElementById("navigation_left_button").href = "javascript:navigation_left_close();"
 	};
 	function navigation_left_close() {
