477c6eb0bf4205203a324ff5a01648b86873e12a
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-05-18 17:35:44 +0800

    Fix detect event

--- assets/navigation/navigation.js
+++ assets/navigation/navigation.js
@@ -20,15 +20,15 @@ Programming Language:	JavaScript/ECMAScript 6/7/8, jQuery 2(.2.4)/3(.3.1)
 			document.getElementById("navigation_left").style.width = "256px";
 		} else {
 			document.getElementById("navigation_left").style.width = "100%";
-		}
+		};
 		document.getElementById("page").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-		document.getElementById("navigation_left_button").href = "javascript:navigation_left_close();"
+		document.getElementById("navigation_left_button").href = "javascript:navigation_left_close();";
 	};
 	function navigation_left_close()
 	{
 		document.getElementById("navigation_left").style.width = null;
 		document.getElementById("page").style.backgroundColor = null;
-		document.getElementById("navigation_left_button").href = "javascript:navigation_left_open();"
+		document.getElementById("navigation_left_button").href = "javascript:navigation_left_open();";
 	};
 
 /*Load Navigation*/
