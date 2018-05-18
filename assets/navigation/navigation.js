8cd09f2eba0fbdbb2de4e30970c07307279c4a5b
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-05-18 18:59:22 +0800

    Test

--- assets/navigation/navigation.js
+++ assets/navigation/navigation.js
@@ -4,14 +4,7 @@ Author:					hugoalh
 Programming Language:	JavaScript/ECMAScript 6/7/8, jQuery 2(.2.4)/3(.3.1)
 ==================================================================================================*/
 var client_resolution_width;
-
-/*Determine Client Resolution Of Width*/
-	function detect_resolution_width()
-	{
-		client_resolution_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-	};
-	detect_resolution_width();
-	window.addEventListener("resize", detect_resolution_width);
+var navigation_left_mode;
 
 /*Navigation Left Control*/
 	function navigation_left_open()
@@ -24,13 +17,27 @@ var client_resolution_width;
 		};
 		document.getElementById("page").style.backgroundColor = "#0000007F";
 		document.getElementById("navigation_left_button").href = "javascript:navigation_left_close();";
+		navigation_left_mode = 1;
 	};
 	function navigation_left_close()
 	{
 		document.getElementById("navigation_left").style.width = null;
 		document.getElementById("page").style.backgroundColor = null;
 		document.getElementById("navigation_left_button").href = "javascript:navigation_left_open();";
+		navigation_left_mode = 0;
 	};
+
+/*Determine Client Resolution Of Width*/
+	function detect_resolution_width()
+	{
+		client_resolution_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+		if (client_resolution_width >= 768 && navigation_left_mode == 1)
+		{
+			navigation_left_close();
+		}
+	};
+	detect_resolution_width();
+	window.addEventListener("resize", detect_resolution_width);
 
 /*Load Navigation*/
 	$(document).ready(function()
