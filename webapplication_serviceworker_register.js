36ab664c2f54e3e282e02924c2969c1ac2e0b942
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-05-29 19:02:56 +0800

    [GHD] 20180529D
    - Fix icons (d)

--- webapplication_serviceworker_register.js
+++ webapplication_serviceworker_register.js
@@ -6,7 +6,7 @@ Source:					https://www.pwabuilder.com/serviceworker
 Programming Language:	JavaScript/ECMAScript 6/7/8
 ==================================================================================================*/
 if (navigator.serviceWorker.controller) {
-	console.log("[hugoalh.github.io Web Application - Service Worker - Register] Actived service worker was found, unnecessary to register again.")
+	console.log("[hugoalh.github.io Web Application - Service Worker - Register] Actived service worker was found, unnecessary to register again.");
 } else {
 	/*Register The Service Worker*/
 		navigator.serviceWorker.register("/webapplication_serviceworker.js", {scope: "/"}).then(function(reg) {
