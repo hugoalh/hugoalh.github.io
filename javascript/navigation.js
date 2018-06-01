c5ded5f593c985af7490e7cff7574629ca82dcbc
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-06-01 13:09:15 +0800

    [GHD] 20180601A
    - Fix navigation

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -8,13 +8,13 @@ var navigation_left_mode;
 
 /*Control, Left*/
 	function navigation_left_open() {
-		document.getElementById("navigation_left").style.width = "256px";
+		document.getElementById("navigation_left").style.left = "0px";
 		document.getElementById("page").style.backgroundColor = "rgba(0,0,0,0.5)";
 		document.getElementById("navigation_left_button").setAttribute("onClick","javascript:navigation_left_close();");
 		navigation_left_mode = 1;
 	};
 	function navigation_left_close() {
-		document.getElementById("navigation_left").style.width = null;
+		document.getElementById("navigation_left").style.left = null;
 		document.getElementById("page").style.backgroundColor = null;
 		document.getElementById("navigation_left_button").setAttribute("onClick","javascript:navigation_left_open();");
 		navigation_left_mode = 0;
