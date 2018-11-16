807ba9f0877ff5d02ae5849074b8b78eb38c0c74
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-11-16 17:13:55 +0800

    20181116V

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -60,7 +60,7 @@ $(function() {
 		$("#navigation_top").load("/navigation/top.html-embed");
 		$("#navigation_left").load("/navigation/left.html-embed", function(response, status, xhr) {
 			if (status == "success") {
-				var websiteversion = $.parseJSON($.getJSON("/package.json");
+				var websiteversion = $.parseJSON($.getJSON("/package.json"));
 				document.getElementById("websiteversion").innerHTML = websiteversion.version;
 				website_urlpageparameter_get();
 				document.getElementById("blur").addEventListener("click", navigation_left_close);
