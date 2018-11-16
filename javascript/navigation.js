c97ab7759777af9299415b66a6c2a20be3f4dcb9
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-11-16 16:33:48 +0800

    20181116R

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -6,10 +6,7 @@ hugoalh.github.io Website Navigation
 		JavaScript/ECMAScript 6/7/8
 		jQuery 3(.3.1)
 ==================================================*/
-var protocol = location.protocol;
-var hostname = location.hostname;
 var navigation_left_mode;
-var needloadpage_parameter;
 
 /*Navigation Left Control*/
 function navigation_left_open() {
@@ -24,11 +21,6 @@ function navigation_left_close() {
 	document.getElementById("navigation_left_button").setAttribute("onClick","javascript:navigation_left_open();");
 	navigation_left_mode = 0;
 };
-function navigation_triggerloadpage(needloadpage_parameter) {
-	var needloadpageurl = protocol + hostname + needloadpage_parameter;
-	history.pushState("", "hugoalh", needloadpageurl);
-	website_pageparameter_get();
-}
 
 /*Determine Device Event*/
 function navigation_deviceevent() {
@@ -40,24 +32,22 @@ function navigation_deviceevent() {
 window.addEventListener("resize", navigation_deviceevent);
 
 /*Website Page Parameter Handle*/
-function website_pageparameter_get() {
-	var browser_url_pageparameter_get = new URLSearchParams(location.search.substring(1));
-	browser_url_pageparameter_get = browser_url_pageparameter_get.get("page");
-	website_pageparameter_load(browser_url_pageparameter_get);
+function website_urlpageparameter_get() {
+	var urlpageparameter_get = new URLSearchParams(location.search.substring(1));
+	urlpageparameter_get = urlpageparameter_get.get("page");
+	website_urlpageparameter_load(urlpageparameter_get);
 };
-function website_pageparameter_load(browser_url_pageparameter) {
-	var pageparameter;
-	if (browser_url_pageparameter == null || browser_url_pageparameter == "404") {
-		pageparameter = "homepage";
-	} else {
-		pageparameter = browser_url_pageparameter;
+function website_urlpageparameter_load(urlpageparameter) {
+	if (urlpageparameter == null || urlpageparameter == "404") {
+		urlpageparameter = "homepage";
+		history.replaceState("", "hugoalh", "/");
 	};
-	var pageparameter_encode = "#navigation_left #menu a[href='/?page=" + pageparameter + "']";
+	var urlpageparameter_encode = "#navigation_left #menu a[href='/?page=" + urlpageparameter + "']";
 	$("#navigation_left #menu a").removeClass("navigation_currentpage");
-	$(pageparameter_encode).addClass("navigation_currentpage");
-	pageparameter = pageparameter.replace(/,/g, "/");
-	pageparameter = pageparameter.replace(/\2c /g, "/");
-	page_needload = "/page/" + pageparameter + ".html-embed";
+	$(urlpageparameter_encode).addClass("navigation_currentpage");
+	urlpageparameter = urlpageparameter.replace(/,/g, "/");
+	urlpageparameter = urlpageparameter.replace(/\2c /g, "/");
+	page_needload = "/page/" + urlpageparameter + ".html-embed";
 	$("#page").load(page_needload, function(response, status, xhr) {
 		if (status == "error") {
 			$("#page").load("/page/404.html-embed");
@@ -70,12 +60,13 @@ $(function() {
 		$("#navigation_top").load("/navigation/top.html-embed");
 		$("#navigation_left").load("/navigation/left.html-embed", function(response, status, xhr) {
 			if (status == "success") {
-				website_pageparameter_get();
+				website_urlpageparameter_get();
 				document.getElementById("blur").addEventListener("click", navigation_left_close);
 				$("#navigation_left #menu a").click(function(event) {
-					event.preventDefault();
-					needloadpage_parameter = this.getAttribute("href");
-					navigation_triggerloadpage(needloadpage_parameter);
+					event.preventDefault()/*Prevent browser to execute default thing*/;
+					var self_href = this.getAttribute("href");
+					history.pushState("", "hugoalh", self_href);
+					website_urlpageparameter_get();
 					navigation_left_close();
 				});
 				$("#coverscreen").css("display","none");
