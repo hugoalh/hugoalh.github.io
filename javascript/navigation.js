c3fc2aa30149398d84b34d65a5c787e05727242c
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-11-16 13:43:18 +0800

    20181116L

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -6,9 +6,7 @@ hugoalh.github.io Website Navigation
 		JavaScript/ECMAScript 6/7/8
 		jQuery 3(.3.1)
 ==================================================*/
-var device_resolution_width;
 var navigation_left_mode;
-var browser_url_pageparameter_get;
 
 /*Navigation Left Control*/
 function navigation_left_open() {
@@ -23,10 +21,18 @@ function navigation_left_close() {
 	document.getElementById("navigation_left_button").setAttribute("onClick","javascript:navigation_left_open();");
 	navigation_left_mode = 0;
 };
+function navigation_triggerloadpage() {
+	var href = $(this).attr("href");
+	var protocol = location.protocol;
+	var hostname = location.hostname;
+	var needloadpageurl = protocol + hostname + href;
+	history.pushState(stateObj, "hugoalh", needloadpageurl);
+	website_pageparameter_get();
+}
 
 /*Determine Device Event*/
 function navigation_deviceevent() {
-	device_resolution_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	var device_resolution_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 	if (device_resolution_width >= 896 && navigation_left_mode == 1) {
 		navigation_left_close();
 	};
@@ -35,19 +41,18 @@ window.addEventListener("resize", navigation_deviceevent);
 
 /*Website Page Parameter Handle*/
 function website_pageparameter_get() {
-	browser_url_pageparameter_get = new URLSearchParams(location.search.substring(1));
+	var browser_url_pageparameter_get = new URLSearchParams(location.search.substring(1));
 	browser_url_pageparameter_get = browser_url_pageparameter_get.get("page");
 	website_pageparameter_load(browser_url_pageparameter_get);
 };
 function website_pageparameter_load(browser_url_pageparameter) {
 	var pageparameter;
-	var pageparameter_encode;
 	if (browser_url_pageparameter == null || browser_url_pageparameter == "404") {
 		pageparameter = "homepage";
 	} else {
 		pageparameter = browser_url_pageparameter;
 	};
-	pageparameter_encode = "#navigation_left #menu a[href='/?page=" + pageparameter + "']";
+	var pageparameter_encode = "#navigation_left #menu a[href='/?page=" + pageparameter + "']";
 	$("#navigation_left #menu a").removeClass("navigation_currentpage");
 	$(pageparameter_encode).addClass("navigation_currentpage");
 	pageparameter = pageparameter.replace(/,/g, "/");
