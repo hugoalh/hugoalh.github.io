e5463c9963a764e95b1a41ca584e5e70223a21dd
hugoalh <32359235+hugoalh@users.noreply.github.com>
2018-11-20 13:13:38 +0800

    20181120A

---
+++ webapplication/progressive_serviceworker-register.js
@@ -0,0 +1,18 @@
+/*==================================================
+Web Application - Service Worker - Register
+	Author
+		hugoalh
+	Program Language
+		JavaScript/ECMAScript 6/7/8
+	Source
+		https://www.pwabuilder.com/serviceworker
+		https://jakearchibald.com/2014/offline-cookbook
+==================================================*/
+if (navigator.serviceWorker.controller) {
+	console.log("[hugoalh.github.io Web Application - Service Worker] Found actived service worker, skipped register process.");
+} else {
+	/*Register Service Worker*/
+	navigator.serviceWorker.register("/webapplication/progressive_serviceworker.js", {scope: "/"}).then(function(reg) {
+		console.log("[hugoalh.github.io Web Application - Service Worker] Service worker registered for scope: "+ reg.scope);
+	});
+};
