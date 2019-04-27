3a74607e90b5ecd9638e35f6f79ca41f24bdb9a3
hugoalh <32359235+hugoalh@users.noreply.github.com>
2019-04-27 18:00:43 +0800

    20190427C

--- script/cookie.js
+++ script/cookie.js
@@ -7,27 +7,27 @@ Cookie
 		Cookie
 ========*/
 /*Set Cookie*/
-function cookie_set(name, value, expiretime) {
-	var domain_set = location.hostname;
-	var expiretime_set = new Date();
-	expiretime_set.setTime(expiretime_set.getTime() + (expiretime * 24 * 60 * 60 * 1000));
-	var expiretime_set_utc = expiretime_set.toUTCString();
-	document.cookie = name + "=" + value + ";expires=" + expiretime_set_utc + ";domain=" + domain_set + ";path=/";
-};
+	function cookie_set(name, value, expiretime) {
+		var domain_set = location.hostname;
+		var expiretime_set = new Date();
+		expiretime_set.setTime(expiretime_set.getTime() + (expiretime * 24 * 60 * 60 * 1000));
+		var expiretime_set_utc = expiretime_set.toUTCString();
+		document.cookie = name + "=" + value + ";expires=" + expiretime_set_utc + ";domain=" + domain_set + ";path=/";
+	};
 
 /*Get Cookie*/
-function cookie_get(cookie_name) {
-	var cookie_name_search = cookie_name + "=";
-	var cookie_decode = decodeURIComponent(document.cookie);
-	var cookie_array = cookie_decode.split(';');
-	for(var i = 0; i < cookie_array.length; i++) {
-		var c = cookie_array[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
+	function cookie_get(cookie_name) {
+		var cookie_name_search = cookie_name + "=";
+		var cookie_decode = decodeURIComponent(document.cookie);
+		var cookie_array = cookie_decode.split(';');
+		for(var i = 0; i < cookie_array.length; i++) {
+			var c = cookie_array[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			};
+			if (c.indexOf(cookie_name_search) == 0) {
+				return c.substring(cookie_name_search.length, c.length);
+			}
 		};
-		if (c.indexOf(cookie_name_search) == 0) {
-			return c.substring(cookie_name_search.length, c.length);
-		}
+		return "";
 	};
-	return "";
-};
--- script/navigation.js
+++ script/navigation.js
@@ -11,73 +11,69 @@ var website_version;
 
 /*Handle navigation left*/
 	/*Open*/
-	function navigation_left_open() {
-		document.getElementById("navigation_left").style.left = "0px";
-		document.getElementById("blur").style.display = "block";
-		document.getElementById("navigation_left_button").setAttribute("onClick","javascript:navigation_left_close();");
-		navigation_left_visible = true;
-	};
+		function navigation_left_open() {
+			document.getElementById("navigation_left").style.left = "0px";
+			document.getElementById("blur").style.display = "block";
+			document.getElementById("navigation_left_button").setAttribute("onClick","javascript:navigation_left_close();");
+			navigation_left_visible = true;
+		};
 	/*Close*/
-	function navigation_left_close() {
-		document.getElementById("navigation_left").style.left = "";
-		document.getElementById("blur").style.display = "";
-		document.getElementById("navigation_left_button").setAttribute("onClick","javascript:navigation_left_open();");
-		navigation_left_visible = false;
-	};
+		function navigation_left_close() {
+			document.getElementById("navigation_left").style.left = "";
+			document.getElementById("blur").style.display = "";
+			document.getElementById("navigation_left_button").setAttribute("onClick","javascript:navigation_left_open();");
+			navigation_left_visible = false;
+		};
 
-/*Determine device resize event*/
-$(window).resize(function() {
-	var device_resolution_width = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
-	if (device_resolution_width >= 896 && navigation_left_visible == true) {
-		navigation_left_close();
-	};
-});
+/*Event to determine device resize*/
+	$(window).resize(function() {
+		var device_resolution_width = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+		if (device_resolution_width >= 896 && navigation_left_visible == true) {
+			navigation_left_close();
+		};
+	});
 
 /*Handle device URL parameter*/
 	/*Parse URL parameter*/
-	function website_urlpageparameter_get() {
-		var urlpageparameter_get = new URLSearchParams(location.search.substring(1));
-		urlpageparameter_get = urlpageparameter_get.get("page");
-		website_urlpageparameter_load(urlpageparameter_get);
-	};
-	/*Handle page*/
-	function website_urlpageparameter_load(urlpageparameter) {
-		if (urlpageparameter == null || urlpageparameter == "homepage" || urlpageparameter == "404") {
-			urlpageparameter = "homepage";
-			history.replaceState("", "hugoalh", "/");
+		function website_urlpageparameter_get() {
+			var urlpageparameter_get = new URLSearchParams(location.search.substring(1));
+			urlpageparameter_get = urlpageparameter_get.get("page");
+			website_urlpageparameter_load(urlpageparameter_get);
 		};
-		var urlpageparameter_encode = "#navigation_left #menu a[href='/?page=" + urlpageparameter + "']";
-		$("#navigation_left #menu a").removeClass("navigation_currentpage");
-		$(urlpageparameter_encode).addClass("navigation_currentpage");
-		urlpageparameter = urlpageparameter.replace(/,/g, "/");
-		urlpageparameter = urlpageparameter.replace(/\2c /g, "/");
-		var page_needload = "/page/" + urlpageparameter + ".html-embed";
-		$("#page").load(page_needload, function(response, status, xhr) {
-			if (status == "error" || status == "timeout" || status == "abort" || status == "parsererror") {
-				$("#page").load("/page/404.html-embed");
+	/*Handle page*/
+		function website_urlpageparameter_load(urlpageparameter) {
+			if (urlpageparameter == null || urlpageparameter == "homepage" || urlpageparameter == "404") {
+				urlpageparameter = "homepage";
+				history.replaceState("", "hugoalh", "/");
 			};
-		}).scrollTop(0);
-	};
-	/*Determine device URL change*/
-	window.onpopstate = function() {
-		website_urlpageparameter_get();
-	};
+			var urlpageparameter_encode = "#navigation_left #menu a[href='/?page=" + urlpageparameter + "']";
+			$("#navigation_left #menu a").removeClass("navigation_currentpage");
+			$(urlpageparameter_encode).addClass("navigation_currentpage");
+			urlpageparameter = urlpageparameter.replace(/,/g, "/");
+			urlpageparameter = urlpageparameter.replace(/\2c /g, "/");
+			var page_needload = "/page/" + urlpageparameter + ".html-embed";
+			$("#page").load(page_needload, function(response, status, xhr) {
+				if (status == "error" || status == "timeout" || status == "abort" || status == "parsererror") {
+					$("#page").load("/page/404.html-embed");
+				};
+			}).scrollTop(0);
+		};
+	/*Event to determine device URL change*/
+		window.onpopstate = function() {
+			website_urlpageparameter_get();
+		};
 
 /*Load navigation*/
-website_urlpageparameter_get();
-$.getJSON("/package.json", function(json_package) {
-	website_version = json_package.version;
-	document.getElementById("websiteversion").innerHTML = website_version;
-});
-document.getElementById("blur").addEventListener("click", navigation_left_close);
-$("#navigation_left #menu a").click(function(event) {
-	event.preventDefault()/*Prevent browser to execute default action*/;
-	var self_href = this.getAttribute("href");
-	history.pushState("", "hugoalh", self_href);
 	website_urlpageparameter_get();
-	navigation_left_close();
-});
-$(document).ready(function() {
-	document.getElementById("initialscreen").style.display = "none";
-	$("#initialscreen").remove();
-});
+	document.getElementById("blur").addEventListener("click", navigation_left_close);
+	$("#navigation_left #menu a").click(function(event) {
+		event.preventDefault();/*Prevent browser to execute default action*/
+		var self_href = this.getAttribute("href");
+		history.pushState("", "hugoalh", self_href);
+		website_urlpageparameter_get();
+		navigation_left_close();
+	});
+	$(document).ready(function() {
+		document.getElementById("initialscreen").style.display = "none";
+		$("#initialscreen").remove();
+	});
