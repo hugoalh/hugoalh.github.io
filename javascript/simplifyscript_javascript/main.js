ba72f2c67e41053e998a40355e86b4340b3c5bae
hugoalh <32359235+hugoalh@users.noreply.github.com>
2019-08-28 17:16:18 +0800

    20190828B

--- javascript/simplifyscript_javascript/main.js
+++ javascript/simplifyscript_javascript/main.js
@@ -1,13 +1,50 @@
 /*
 Simplify Script - JavaScript - Main
-    Author
-        hugoalh
-    Language
-        JavaScript/ECMAScript 6
+	Author
+		hugoalh
+	Language
+		JavaScript/ECMAScript 6
 */
-const $SSBr = {};
-$SSBr["Version"] = function() {
-};
-$SSBr["BrowserStorage"] = import("./module/browserstorage.js");
-$SSBr["Math"] = import("./module/math.js");
-export {$SSBr};
+/* Define library index */
+	const SimplifyScript_JavaScript = {};
+
+/* Library core */
+	/* Version */
+	let LibraryVersion = "0.0.1";
+	SimplifyScript_JavaScript["Version"] = function(VersionSyntax = -1) {
+		let loot = LibraryVersion.split(".");
+		if (VersionSyntax < -1 || VersionSyntax > 4) {
+			VersionSyntax = -1;
+		};
+		switch (VersionSyntax) {
+			case -1:
+				return LibraryVersion;
+				break;
+			case 0:
+				return loot[0];
+				break;
+			case 1:
+				return loot[1];
+				break;
+			case 2:
+				return loot[2];
+				break;
+			case 3:
+				return loot[0] + "." + loot[1];
+				break;
+			case 4:
+				return loot[1] + "." + loot[2];
+				break;
+		};
+	};
+
+/* Library module */
+	SimplifyScript_JavaScript["BrowserStorage"] = import("./module/browserstorage.js");
+	SimplifyScript_JavaScript["Math"] = import("./module/math.js");
+
+/* Package library as one module */
+	const $SSJS = SimplifyScript_JavaScript
+	export {
+		SimplifyScript_JavaScript,
+		$SSJS
+	};
--- javascript/simplifyscript_javascript/module/browserinformation.js
+++ javascript/simplifyscript_javascript/module/browserinformation.js
@@ -7,36 +7,58 @@ Simplify Script - JavaScript - Browser Information
 */
 const BrowserInformation = {};
 let Agent = navigator.userAgent;
+let BrandName;
 let Language = (navigator.language || navigator.userLanguage || navigator.browserLanguage || navigator.systemLanguage).toLowerCase();
 let Device;
 let OperatingSystem;
 let DataList = {
 	"Bot": [
 		"bot",
-		"Bot",
-		"BOT",
 		"generator",
-		"Generator",
 		"http://",
 		"https://",
 		".com",
 		".net",
-		"Google Web Preview",
-		"Google Page Speed Insight",
-		"BingPreview",
-		"crawler",
-		"Crawler"
+		"google web preview",
+		"google page speed insight",
+		"bingpreview",
+		"crawler"
 	]
 };
 
+/* Determine browser brand name */
+if (Agent.indexOf("Firefox") != -1) {
+	BrandName = "Mozilla Firefox";
+} else if (Agent.indexOf("Opera") != -1 || Agent.indexOf("OPR") != -1) {
+	BrandName = "Opera";
+} else if (Agent.indexOf("MSIE") != -1 || Agent.indexOf("Trident") != -1) {
+	BrandName = "Microsoft Internet Explorer";
+} else if (Agent.indexOf("Edge") != -1) {
+	BrandName = "Microsoft Edge";
+} else if (Agent.indexOf("Chrome") != -1) {
+	BrandName = "Google Chrome";
+} else if (Agent.indexOf("Safari") != -1) {
+	BrandName = "Apple Safari";
+} else {
+	BrandName = "Unknown";
+}
+
 /* Determine bot */
 for (let index = 0; index < DataList.Bot.length; index++) {
-	if (Agent.search(DataList.Bot[index]) != -1) {
+	if (Agent.toLowerCase().search(DataList.Bot[index]) != -1) {
 		Device = "Bot";
 	};
 };
-BrowserInformation.Device.IsBot = function() {
-	if (Device == "Bot") {
+
+BrowserInformation.Agent = function(index = null) {
+	if (index == null) {
+		return Agent;
+	};
+};
+BrowserInformation.Device = function(index = null) {
+	if (index == null) {
+		return Device;
+	} else if (Device.search(index) != -1) {
 		return true;
 	} else {
 		return false;
